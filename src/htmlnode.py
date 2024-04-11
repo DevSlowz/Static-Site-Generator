b562cb48567ea4ff9885809e4e6a042a08aecf54
DevSlowz <msvieira021@gmail.com>
2024-04-11 08:05:29 -0400

    Adjusted ParentNode tohtml

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -41,18 +41,19 @@ class LeafNode(HTMLNode):
     
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag=None, children=None, props=None):
+    def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        result = ""
         if self.tag is None:
             raise ValueError("Invalid HTML: no tag")
-        if len(self.children) == 0:
+        if self.children is None:
             raise ValueError("Invalid HTML: no children")
+        children_html = ""
         for child in self.children:
-            result += child.to_html()
-        return f'<{self.tag}>{result}</{self.tag}>'
-    
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
     def __repr__(self):
-        return f"ParentNode({self.tag}, {self.children}, {self.props})"
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
