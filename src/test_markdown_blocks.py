992c6ee8db47c29df68a94192681822c41a262a7
DevSlowz <msvieira021@gmail.com>
2024-04-17 17:24:42 -0400

    test file for markdown block created

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,12 @@
+import unittest
+from markdown_blocks import markdown_to_blocks
+
+class TestInlineMarkdown(unittest.TestCase):
+
+    def test_demo(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
