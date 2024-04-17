098143caa2d2e3caeda3514aacc8f9bb9c8c00aa
DevSlowz <msvieira021@gmail.com>
2024-04-17 17:49:19 -0400

    Completed markdown strings to block conversion

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,2 +1,13 @@
+import re
+
+# Takes an entire document as a markdown string and returns blocks
 def markdown_to_blocks(markdown) :
-    pass
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
