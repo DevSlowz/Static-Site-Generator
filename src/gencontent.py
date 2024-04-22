534a6ed6f24282d0baa2ca48618d82659cc1b867
DevSlowz <msvieira021@gmail.com>
2024-04-22 16:43:16 -0400

    Add extract_title and generate_page extract_title simply extracts the h1 title from a markdown file generate_page reads the content of the markdown and template file and converts it to html It will then replace the content in the template file

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,34 +1,43 @@
 import os
+import re
 from markdown_blocks import markdown_to_html_node
 
+# Grab the h1 text from the markdown file and return it 
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
 
+
+# 
 def generate_page(from_path, template_path, dest_path):
-    print(f" * {from_path} {template_path} -> {dest_path}")
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
+
+    # Open file from 'from_path' -> read the contents -> store in variable
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
     from_file.close()
 
+    # Open file from 'template_path' -> read the contents -> store in variable
     template_file = open(template_path, "r")
     template = template_file.read()
     template_file.close()
 
+    # Convert markdown document to html nodes
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
 
+    # Grab title from file 
     title = extract_title(markdown_content)
+
+    # Replace template placeholders with real data
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
+    to_file = open(dest_path, 'w')
     to_file.write(template)
-
-
-def extract_title(md):
-    lines = md.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line[2:]
-    raise ValueError("No title found")
