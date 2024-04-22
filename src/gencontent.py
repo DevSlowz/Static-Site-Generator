bed9db9616295e6bb5298cd409ec6448d5aecfa7
DevSlowz <msvieira021@gmail.com>
2024-04-22 18:54:17 -0400

    complete generate_pages_recursive generate_pages_recursive will now scan the comple folder and collect all markdown files and convert them according to the template provided

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,6 @@
 import os
 import re
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 # Grab the h1 text from the markdown file and return it 
@@ -11,7 +12,7 @@ def extract_title(markdown):
     raise ValueError("No title found")
 
 
-# 
+# Generate a single HTML page
 def generate_page(from_path, template_path, dest_path):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
 
@@ -42,14 +43,19 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, 'w')
     to_file.write(template)
 
-    # crawl every entry in the dir_path_content directory and generate a new .html page for each markdown file it finds.
-    def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-        
-        # Crawl entries in dir_path_content
-        
 
+# Recursively generate HTML pages for markdown files in a directory
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
+    # Crawl entries in dir_path_content
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
         # Generate a new .html page for each markdown file
-
-
-
-        # Write to the dest_dir_path using the template_path template
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            # Write to the dest_dir_path using the template_path template
+            generate_page(from_path, template_path, dest_path)
+        else:
+            # Write to the dest_dir_path using the template_path template
+            generate_pages_recursive(from_path, template_path, dest_path)
